c038b1251cc1d664722173247d1649505aad921a
Ben Chatelain <ben@octop.ad>
2024-04-28 11:19:25 -0600

    🎨 Format kotlin

--- android/app/src/test/java/at/phatbl/androidapp/ExampleUnitTest.kt
+++ android/app/src/test/java/at/phatbl/androidapp/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package at.phatbl.androidapp
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
